7ab50413efd274cdea4785d1304ff690e0311846
diennv <diennv@ierp.vn>
2024-12-11 13:28:38 +0700

    Day 1 - create angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ToDoListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  
+}
